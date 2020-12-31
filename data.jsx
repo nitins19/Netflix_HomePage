f91f10e26d7a2eaf26832797084fb7feca887ba2
Nitin Saxena <72120832+nitins19@users.noreply.github.com>
2020-12-31 21:23:47 +0530

    array of objects

---
+++ data.jsx
@@ -0,0 +1,28 @@
+let data=[
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp1811394.jpg",
+        series_name:"Narcos",
+         link:"https://www.netflix.com/in/title/80025172"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp7948320.jpg",
+        series_name:"The Queen's Gambit",
+        link:"https://www.netflix.com/in/title/80234304"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        series_name:"DARK",
+         link:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp2590143.jpg",
+        series_name:"Friends",
+         link:"https://www.netflix.com/in/title/70153404"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp2358417.jpg",
+        series_name:"Money Heist",
+         link:"https://www.netflix.com/in/title/80192098"
+    }
+];
+export default data;
