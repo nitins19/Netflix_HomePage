b910884e9751fdc29c35658355ab6e7bb6fff7e9
Nitin Saxena <72120832+nitins19@users.noreply.github.com>
2021-01-01 17:35:11 +0530

    Updated App file

--- App.jsx
+++ App.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import Series from "./Series"
 import data from "./data.jsx";
+const Card= ()=>
+(
+    data.map((val,index)=>{
+        return(
+            <Series 
+            key={val.id}
+            imgsrc={val.imgsrc}
+            series_name={val.series_name}
+            link={val.link}     
+            />
+        );
+    })
+);
 
 function App()
 {
     return(<>
     
     <h1 className="heading">List of Top 5 Netflix Series in 2020 </h1>
-    {data.map((val,index)=>{
-        return(
-            <Series 
-            imgsrc={val.imgsrc}
-            series_name={val.series_name}
-            link={val.link}     
-            />
-        );
-    })}
+    <Card/>
+   
     </>);
 }
 export default App;
